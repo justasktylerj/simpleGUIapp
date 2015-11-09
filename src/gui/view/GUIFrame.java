6ed920f40c621b7f00e7dc8cadf49a8987d24954
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-11-09 09:56:29 -0700

    layout

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -12,17 +12,26 @@ import gui.controller.GUIController;
 public class GUIFrame extends JFrame
 {
 	private GUIController baseController;
+	private GUIPanel basePanel;
 	
 	public GUIFrame(GUIController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GUIPanel(baseController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(basePanel);  //installs the panel in the frame
 		this.setSize(400,400);
+		this.setTitle("silly app!"); // find a good size for app
 		this.setResizable(false); //can't change size of window
-		this.setVisible(true);
+		this.setVisible(true);  //must be last line of setupFrame
+	}
+	
+	public GUIController getBaseController()
+	{
+		return baseController;
 	}
 }
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,14 +1,52 @@
 package gui.view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import gui.controller.GUIController;
+import javax.swing.SpringLayout; //for layout
 
 public class GUIPanel extends JPanel
 {
 	private GUIController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIController baseController)
 	{
 		this.baseController = baseController;
+		
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Please do not click the button");
+		firstTextField = new JTextField("words can be type here");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
 	}
-}
+	
+	/**
+	 * helper method to load all GUI components into the panel
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
+	}
+	
+	private void setupLayout() // FOR crap code
+	{
+		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 83, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -196, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 63, SpringLayout.SOUTH, firstButton);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 129, SpringLayout.WEST, this);
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}	
+
